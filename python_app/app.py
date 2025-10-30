5002b467c6061814d5f8ad05bc11e47a4cf3d21c
Lasya <digadarilasya@gmail.com>
2025-10-30 06:47:00 +0000

    4

---
+++ python_app/app.py
@@ -0,0 +1,20 @@
+
+
+
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/apikey')
+def get_key():
+    api_key = os.getenv("API_KEY", "No API key found")
+    return jsonify({"api_key": api_key})
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Flask app running. Use /apikey to get the API key."})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
